cdbdb5d72e2e703a892f73645911b1605ad82522
Prasant Samal <samal_prasant123@yahoo.co.in>
2018-04-05 23:13:05 -0400

    Two way Data Binding
    [(ngModel)]="data" but do add and import FormsModule in AppModule

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,7 +11,7 @@ export class ServersComponent implements OnInit {
 
   serverCreationStatus = 'No server was created.';
 
-  serverName = '';
+  serverName = 'Test';
 
   constructor() {
     setTimeout(() => {
